fd0eadb41a0f1f5ce34404e44846921569fba932
Pnonato <like.nonato.marrara@gmail.com>
2025-10-28 20:12:17 -0300

    feat: logica calcula imc, encaminhamento via intent para a proxima tela

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3047059/imfitplus/PersonalDataActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3047059/imfitplus/PersonalDataActivity.kt
@@ -1,6 +1,10 @@
 package br.edu.ifsp.scl.ads.prdm.sc3047059.imfitplus
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +15,23 @@ class PersonalDataActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_personal_data)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val heigth_et = findViewById<EditText>(R.id.height_et)
+        val weight_et = findViewById<EditText>(R.id.weight_et)
+        val calculate_bt = findViewById<Button>(R.id.calculate_bt)
+
+
+        calculate_bt.setOnClickListener {
+            val final_height = heigth_et.text.toString().toDoubleOrNull()
+            val final_weight = weight_et.text.toString().toDoubleOrNull()
+            if (final_weight != null && final_height != null && final_weight > 0){
+                val imc = final_weight / (final_height * final_height)
+                val intent = Intent(this, ImcResult::class.java)
+                intent.putExtra("IMC_RESULT", imc)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Preencha os campos corretamente", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
