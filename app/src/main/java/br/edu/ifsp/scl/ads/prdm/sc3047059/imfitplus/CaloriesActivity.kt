e6fa1d7045e843b0d6b054ebb0cb79ca0c54001c
Pnonato <like.nonato.marrara@gmail.com>
2025-10-28 22:31:15 -0300

    feat: recuperando dados enviados por intent e implementando logica para calcular TMB

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3047059/imfitplus/CaloriesActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3047059/imfitplus/CaloriesActivity.kt
@@ -1,16 +1,41 @@
 package br.edu.ifsp.scl.ads.prdm.sc3047059.imfitplus
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class CaloriesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_calories)
+
+        val weight = intent.getDoubleExtra("WEIGHT", 0.0)
+        val height = intent.getDoubleExtra("HEIGHT", 0.0)
+        val ageStr = intent.getStringExtra("AGE")
+        val age = ageStr?.toIntOrNull() ?: 0
+        val gender = intent.getStringExtra("GENDER")
+
+        val calculateTmbBt = findViewById<Button>(R.id.calculate_tmb_bt)
+        val idealWeightBt = findViewById<Button>(R.id.ideal_weight_bt)
+        val tmbResultTv = findViewById<TextView>(R.id.tmb_result_tv)
+
+        calculateTmbBt.setOnClickListener {
+            if (weight > 0 && height > 0 && age > 0 && gender != null){
+                val tmb = if (gender == "M") {
+                    66 + (13.7 * weight) + (5 * height * 100) - (6.8 * age)
+                } else {
+                    655 + (9.6 * weight) + (1.8 * height * 100) - (4.7 * age)
+                }
+                tmbResultTv.text = "%.0f kcal/dia".format(tmb)
+                tmbResultTv.textSize = 36f
+            } else {
+                Toast.makeText(this, "Dados incompletos para calcular TMB", Toast.LENGTH_LONG).show()
+            }
+        }
 
     }
 }
