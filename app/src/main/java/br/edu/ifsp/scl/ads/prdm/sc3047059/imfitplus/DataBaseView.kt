6c6563478e38624261416e1226e97f63e992c9c5
Pnonato <like.nonato.marrara@gmail.com>
2025-12-09 19:46:52 -0300

    feat: adicionando nova tela para a exibicao dos dados dos usuarios e do historico

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3047059/imfitplus/DataBaseView.kt
@@ -0,0 +1,68 @@
+package br.edu.ifsp.scl.ads.prdm.sc3047059.imfitplus
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import br.edu.ifsp.scl.ads.prdm.sc3047059.imfitplus.model.AppDatabase
+import kotlinx.coroutines.launch
+
+class DataBaseView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_data_base_view)
+
+        val usersTv = findViewById<TextView>(R.id.users_tv)
+        val historyTv = findViewById<TextView>(R.id.history_tv)
+
+        val db = AppDatabase.getDatabase(this)
+        val userDao = db.usuarioDao()
+        val historyDao = db.historyDao()
+
+        lifecycleScope.launch {
+
+            val users = userDao.getUsers()
+            val usersText = buildString {
+                append("Usuários cadastrados:\n\n")
+                for (u in users) {
+
+                    val alturaFormatada = String.format("%.2f m", u.altura)
+                    val pesoFormatado = String.format("%.1f kg", u.peso)
+
+                    append("ID: ${u.id}\n")
+                    append("Nome: ${u.nome}\n")
+                    append("Idade: ${u.idade} anos\n")
+                    append("Sexo: ${u.sexo}\n")
+                    append("Altura: $alturaFormatada\n")
+                    append("Peso: $pesoFormatado\n")
+                    append("Nível atividade: ${u.nivelAtividade}\n")
+                    append("\n----------------------\n\n")
+                }
+            }
+            usersTv.text = usersText
+
+            val historyList = historyDao.getAllHistory()
+            val historyText = buildString {
+                append("Histórico:\n\n")
+                for (h in historyList) {
+
+                    val imcFormatado = String.format("%.2f", h.imc)
+                    val tmbFormatado = String.format("%.0f kcal/dia", h.tmb)
+                    val pesoIdealFormatado = String.format("%.1f kg", h.idealWeight)
+
+                    append("ID: ${h.id}\n")
+                    append("Nome: ${h.userName}\n")
+                    append("IMC: $imcFormatado\n")
+                    append("Categoria IMC: ${h.imcCategory}\n")
+                    append("TMB (gasto): $tmbFormatado\n")
+                    append("Peso ideal: $pesoIdealFormatado\n")
+                    append("\n----------------------\n\n")
+                }
+            }
+            historyTv.text = historyText
+        }
+    }
+}
